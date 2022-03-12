87e88164ce011426e38370e87a374ba245a1ade6
FATEMASAYED25 <batasayed25@gmail.com>
2022-03-12 20:48:45 +0200

    Update ListBooks.js

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -30,7 +30,7 @@ state ={
                       <li key={book.id}>
                         <div className="book" >
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193,backgroundImage: `url(${book.imageLinks})` }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 193,backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                             <div className="book-shelf-changer">
                               <select onChange={(e)=>{(book.shelf) = e.target.value}}>
                                 <option value="move" disabled>Move to...</option>
@@ -60,7 +60,7 @@ state ={
                       <li key={book.id}>
                         <div className="book" >
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks})` }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                             <div className="book-shelf-changer">
                             <select onChange={(e)=>{(book.shelf) = e.target.value}}>
                                 <option value="move" disabled>Move to...</option>
@@ -89,7 +89,7 @@ state ={
                       <li key={book.id}>
                         <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks})` }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                             <div className="book-shelf-changer">
                             <select onChange={(e)=>{(book.shelf) = e.target.value}}>
                                 <option value="move" disabled>Move to...</option>
