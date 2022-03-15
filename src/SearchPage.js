008301a7b904bae816d247ec27c9fcc02ffaf084
FATEMASAYED25 <batasayed25@gmail.com>
2022-03-15 14:00:07 +0200

    Update SearchPage.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,38 +1,33 @@
 import React, { Component } from 'react'
-import axios from 'axios';
+import * as BooksAPI from './BooksAPI';
 import { Link } from "react-router-dom";
 class SearchPage extends Component {
   state = {
     query: "",
-  
+
   }
-
-
-
-
+  async componentDidMount() {
+   const allBooks = await BooksAPI.search(this.state.query);
+   this.setState({allBooks});
+   console.log(this.state.allBooks)
+  }
   // changing the state depends on the value wich the input has written down
   //then set state depending on the search query 
   changeHandler = (e) => {
 
     this.setState({
       query: e.currentTarget.value
-    })
- 
-
-    
-  }
+    })}
 
   render() {
 
     const { query} = this.state;
-    const {books} =this.props;
+    const {search} =this.props;
 
     // filter the books depends on the new value has written down in the query state 
     const filterBooks= query ==="" 
-    ? books
-    : books.filter((c)=>(
-        c.title.toLowerCase().includes(query.toLowerCase())
-    ))
+    ? []
+    : search(query)
 
     return (
       <div className="search-books">
@@ -60,10 +55,10 @@ class SearchPage extends Component {
                       <div className="book-shelf-changer">
                         <select onChange={(e)=>{(book.shelf) = e.target.value}}>
                           <option value="move" disabled>Move to...</option>
+                          <option value="none">None</option>
                           <option value="currentlyReading">Currently Reading</option>
                           <option value="wantToRead">Want to Read</option>
                           <option value="read">Read</option>
-                          <option value="none">None</option>
                         </select>
                       </div>
                     </div>
