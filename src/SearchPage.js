3877fbe534b975bfa638945550e9194203be02f8
FATEMASAYED25 <batasayed25@gmail.com>
2022-03-18 08:59:03 +0200

    Update SearchPage.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import * as BooksAPI from './BooksAPI';
 import { Link } from "react-router-dom";
 class SearchPage extends Component {
   state = {
-    query: "",
-
-  }
-  async componentDidMount() {
-   const allBooks = await BooksAPI.search(this.state.query);
-   this.setState({allBooks});
-   console.log(this.state.allBooks)
+    books: [],
+   
   }
   // changing the state depends on the value wich the input has written down
   //then set state depending on the search query 
-  changeHandler = (e) => {
+  //then if the query = the name of the book or thubnail inoke the search function wich it setstate the books arry with the required books 
+ onSearch = async (e) => {
+  const query = e.target.value;
+ 
+     let booksResult = await BooksAPI.search(query);
+      booksResult.map((found) => {
+         this.props.books.find((book) => book.id === found.id);
+      
+      });
+      this.setState({ books: booksResult });
+  }
 
-    this.setState({
-      query: e.currentTarget.value
-    })}
 
   render() {
 
-    const { query} = this.state;
-    const {search} =this.props;
+    const { books} = this.state;
+    const {updateShelf}=this.props
 
     // filter the books depends on the new value has written down in the query state 
-    const filterBooks= query ==="" 
-    ? []
-    : search(query)
+  
 
     return (
       <div className="search-books">
@@ -37,8 +37,7 @@ class SearchPage extends Component {
 
             <input type="text"
               placeholder="Search by title or author"
-              value={query}
-              onChange={this.changeHandler}
+              onChange={this.onSearch}
             />
 
           </div>
@@ -46,14 +45,16 @@ class SearchPage extends Component {
         <div className="search-books-results">
 
           <ol className="books-grid">
-            {filterBooks.map(book => {
+            {books.map(book => {
               return (
                 <li key={book.id}>
                   <div className="book">
                     <div className="book-top">
                       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                       <div className="book-shelf-changer">
-                        <select onChange={(e)=>{(book.shelf) = e.target.value}}>
+                      <select value={book.shelf}
+                           onChange={(e)=>updateShelf(book,e.target.value)}
+                              >
                           <option value="move" disabled>Move to...</option>
                           <option value="none">None</option>
                           <option value="currentlyReading">Currently Reading</option>
