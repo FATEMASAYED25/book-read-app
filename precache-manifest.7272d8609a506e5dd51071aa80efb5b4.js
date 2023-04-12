5c5a71b67f33f4b938671b223e7968ed9446a82f
FATEMASAYED25 <batasayed25@gmail.com>
2023-04-12 08:56:04 +0200

    Updates

---
+++ precache-manifest.7272d8609a506e5dd51071aa80efb5b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e783d724a3f2a68ceb8f",
+    "url": "/shoes-store/static/css/main.b06a8f60.chunk.css"
+  },
+  {
+    "revision": "e783d724a3f2a68ceb8f",
+    "url": "/shoes-store/static/js/main.e783d724.chunk.js"
+  },
+  {
+    "revision": "769b8adc1d4a6e1cda42",
+    "url": "/shoes-store/static/js/1.769b8adc.chunk.js"
+  },
+  {
+    "revision": "4123157b406b0a08053b",
+    "url": "/shoes-store/static/js/runtime~main.4123157b.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/shoes-store/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/shoes-store/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/shoes-store/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "045e463be9d4517d8c50fa89685db9b0",
+    "url": "/shoes-store/index.html"
+  }
+];
